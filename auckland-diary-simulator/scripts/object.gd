ac0507b7bf98f6ef5a7abc62c91e97399dfabc05
dhonburi <155067007+dhonburi@users.noreply.github.com>
2025-08-21 15:27:43 +1200

    DragAndDropPrototype

---
+++ auckland-diary-simulator/scripts/object.gd
@@ -0,0 +1,50 @@
+extends Node2D
+
+var draggable = false
+var is_inside_dropable = false
+var body_ref
+var offset: Vector2
+var initialPos: Vector2
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if draggable:
+		if Input.is_action_just_pressed("click"):
+			initialPos = global_position
+			offset = get_global_mouse_position() - global_position
+			global.is_dragging = true
+		if Input.is_action_pressed("click"):
+			global_position = get_global_mouse_position() - offset
+		elif Input.is_action_just_released("click"):
+			global.is_dragging = false
+			var tween = get_tree().create_tween()
+			if is_inside_dropable:
+				tween.tween_property(self, "position", body_ref.position, 0.2).set_ease(Tween.EASE_OUT)
+			else:
+				tween.tween_property(self, "global_position", global_position + Vector2(0, 50), 0.1).set_ease(Tween.EASE_IN)
+			
+
+
+func _on_area_2d_mouse_entered() -> void:
+	if !global.is_dragging:
+		draggable = true
+		scale = Vector2(1.05, 1.05)
+
+
+func _on_area_2d_mouse_exited() -> void:
+	if !global.is_dragging:
+		draggable = false
+		scale = Vector2(1, 1)
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group('dropable'):
+		is_inside_dropable = true
+		body.modulate = Color(Color.WEB_GRAY, 1)
+		body_ref = body
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.is_in_group('dropable'):
+		is_inside_dropable = false
+		body.modulate = Color(Color.LIGHT_GRAY, 0.7)
