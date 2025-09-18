dc41eb05aecfd70e14973be2efe1112ccee15e3d
dhonburi <155067007+dhonburi@users.noreply.github.com>
2025-09-18 20:59:03 +1200

    Next day button says day count

--- auckland-diary-simulator/scripts/next_day_button.gd
+++ auckland-diary-simulator/scripts/next_day_button.gd
@@ -1,5 +1,6 @@
 extends Button
 
+@onready var label: Label = $Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,7 +9,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	label.text = "START DAY " + str(GameManager.dayCount + 1)
 
 func _on_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/dayScreen.tscn")
