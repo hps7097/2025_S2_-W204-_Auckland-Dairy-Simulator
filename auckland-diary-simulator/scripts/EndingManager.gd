8ccd278980b52cd111520ed9f10eec9e20629b86
hps7097 <hps7097@autuni.ac.nz>
2025-09-29 12:35:50 +1300

    Update EndingManager.gd

--- auckland-diary-simulator/scripts/EndingManager.gd
+++ auckland-diary-simulator/scripts/EndingManager.gd
@@ -1,20 +1,20 @@
 # MADE WITH CHATGPT
 
-# EndingManager.gd
 extends Node
 
-@onready var game_state = get_tree().root.get_node("Main/GameState")
+func determine_ending() -> String:
+    if GameState.get_flag("gang_path"):
+        return "gang"
+    elif GameState.get_flag("lawful_path"):
+        return "lawful"
+    else:
+        return "neutral"
 
-func trigger_ending(ending_key: String):
-	var scene = preload("res://scenes/EndingScreen.tscn").instantiate()
-	get_tree().root.add_child(scene)
-	match ending_key:
-		"lawful": scene.set_text("You upheld the law…")
-		"gang": scene.set_text("You fell into crime…")
-		"neutral": scene.set_text("You stayed out of trouble…")
+func trigger_ending():
+    var ending_key = determine_ending()
+    var path = "res://scenes/UI/Ending_%s.tscn" % ending_key
+    if ResourceLoader.exists(path):
+        get_tree().change_scene_to_file(path)
+    else:
+        push_warning("Missing ending scene: %s" % path)
 
-
-func show_ending(text: String):
-	var popup = preload("res://scenes/popup_text.tscn").instantiate()
-	popup.set_text(text)
-	get_tree().root.add_child(popup)
