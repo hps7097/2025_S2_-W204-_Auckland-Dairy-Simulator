9f2c46cff47dc7d6cabf80979b9a6ff3c239b23d
hps7097 <hps7097@autuni.ac.nz>
2025-10-12 22:54:21 +1300

    Update test_game_state.gd

--- auckland-diary-simulator/tests/test_game_state.gd
+++ auckland-diary-simulator/tests/test_game_state.gd
@@ -5,25 +5,19 @@ var gs
 func before_each():
     gs = load("res://scripts/GameState.gd").new()
 
-func test_replay_tracking():
+func test_start_record_and_end():
     gs.start_new_run()
-    gs.record_choice("feed_cow")
-    gs.set_ending("good")
+    gs.record_choice("test_choice")
+    gs.record_event("test_event")
+    gs.set_ending("test_end")
     assert_eq(gs.runs.size(), 1)
-    assert_eq(gs.runs[0]["ending"], "good")
+    assert_eq(gs.runs[0]["ending"], "test_end")
 
-func test_save_and_load_cycle():
+func test_save_and_load():
     gs.start_new_run()
-    gs.record_choice("sleep")
-    gs.set_ending("neutral")
-    var save_ok = gs.save_to_file("unittest_save")
-    assert_true(save_ok)
+    gs.set_ending("save_test_end")
+    assert_true(gs.save_to_file("unittest_save"))
     var gs2 = load("res://scripts/GameState.gd").new()
-    var load_ok = gs2.load_from_file("unittest_save")
-    assert_true(load_ok)
+    assert_true(gs2.load_from_file("unittest_save"))
     assert_eq(gs2.runs.size(), 1)
 
-func test_flag_handling():
-    gs.set_flag("gang_path", true)
-    assert_true(gs.get_flag("gang_path"))
-    assert_false(gs.get_flag("lawful_path"))
