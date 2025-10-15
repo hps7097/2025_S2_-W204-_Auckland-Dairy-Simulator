c49e54e87bfeecaf306feade285109109b755ad9
hps7097 <hps7097@autuni.ac.nz>
2025-10-15 23:39:12 +1300

    Update test_game_state.gd

--- auckland-diary-simulator/tests/test_game_state.gd
+++ auckland-diary-simulator/tests/test_game_state.gd
@@ -1,11 +1,14 @@
+# MADE WITH CHATGPT
+
+# tests/test_game_state.gd
 extends "res://addons/gut/test.gd"
 
 var gs
 
-func before_each():
+func before_each() -> void:
     gs = load("res://scripts/GameState.gd").new()
 
-func test_start_record_and_end():
+func test_start_record_and_end() -> void:
     gs.start_new_run()
     gs.record_choice("test_choice")
     gs.record_event("test_event")
@@ -13,7 +16,7 @@ func test_start_record_and_end():
     assert_eq(gs.runs.size(), 1)
     assert_eq(gs.runs[0]["ending"], "test_end")
 
-func test_save_and_load():
+func test_save_and_load_cycle() -> void:
     gs.start_new_run()
     gs.set_ending("save_test_end")
     assert_true(gs.save_to_file("unittest_save"))
