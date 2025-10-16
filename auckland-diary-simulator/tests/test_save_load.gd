29835186a8ca1c4baf3c46c19583faca2edc2bb0
hps7097 <hps7097@autuni.ac.nz>
2025-10-16 18:29:31 +1300

    Update test_save_load.gd

--- auckland-diary-simulator/tests/test_save_load.gd
+++ auckland-diary-simulator/tests/test_save_load.gd
@@ -1,21 +1,16 @@
 # MADE WITH CHATGPT
-# tests/test_save_load.gd
 extends "res://addons/gut/test.gd"
 
-var gs
+func test_save_and_load_cycle():
+	var gs = GameState.new()
+	gs.start_new_run()
+	gs.record_choice("test_choice")
+	gs.set_ending("neutral")
+	gs.save_to_file("temp_test")
+	
+	var gs2 = GameState.new()
+	var ok = gs2.load_from_file("temp_test")
+	assert_true(ok)
+	assert_eq(gs2.runs.size(), 1)
 
-func before_each() -> void:
-    gs = load("res://scripts/GameState.gd").new()
-    # Ensure we have one save to list
-    gs.start_new_run()
-    gs.set_ending("save_list_test")
-    gs.save_to_file("test_save_for_listing")
-
-func test_list_saves_contains_testfile() -> void:
-    var saves = gs.list_saves()
-    assert_true("test_save_for_listing" in saves)
-
-func test_load_saved_file_works() -> void:
-    var gs2 = load("res://scripts/GameState.gd").new()
-    assert_true(gs2.load_from_file("test_save_for_listing"))
 
