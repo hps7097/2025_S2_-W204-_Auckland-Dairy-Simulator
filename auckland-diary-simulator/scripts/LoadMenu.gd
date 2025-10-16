58b3fbc75610ede1d80527be1dba02eed055c3fa
hps7097 <hps7097@autuni.ac.nz>
2025-10-16 18:24:28 +1300

    Refactor LoadMenu script for improved readability

--- auckland-diary-simulator/scripts/LoadMenu.gd
+++ auckland-diary-simulator/scripts/LoadMenu.gd
@@ -1,29 +1,27 @@
 # MADE WITH CHATGPT
-extends Control
+extends VBoxContainer
 
-@onready var save_list := $ScrollContainer/SaveList
+@onready var save_list = $ScrollContainer/SaveList
 
-func _ready() -> void:
-    populate_save_list()
+func _ready():
+	_refresh_save_list()
 
-func populate_save_list() -> void:
-    save_list.queue_free_children()
-    var saves := GameState.list_saves()
-    for filename in saves:
-        var button := Button.new()
-        button.text = filename
-        button.connect("pressed", Callable(self, "_on_load_pressed").bind(filename))
-        save_list.add_child(button)
+func _refresh_save_list():
+	save_list.queue_free_children()
+	var saves = GameState.list_saves()
+	for s in saves:
+		var btn = Button.new()
+		btn.text = s
+		btn.connect("pressed", Callable(self, "_on_load_pressed").bind(s))
+		save_list.add_child(btn)
 
-func _on_load_pressed(filename: String) -> void:
-    var success := GameState.load_from_file(filename)
-    if success:
-        get_tree().change_scene_to_file("res://scenes/MainMenu.tscn")
-    else:
-        _show_error_popup("Failed to load save: " + filename)
+func _on_load_pressed(save_name: String):
+	var ok = GameState.load_from_file(save_name)
+	if ok:
+		get_tree().change_scene("res://scenes/GameScene.tscn")
+	else:
+		push_error("Failed to load save: " + save_name)
 
-func _show_error_popup(msg: String) -> void:
-    var popup := AcceptDialog.new()
-    popup.dialog_text = msg
-    add_child(popup)
-    popup.popup_centered()
+func _on_Back_pressed():
+	get_tree().change_scene("res://scenes/MainMenu.tscn")
+
