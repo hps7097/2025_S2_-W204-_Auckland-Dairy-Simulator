53618175733454acd1131d570365e3974a4aeec5
dhonburi <dhonlao2005@gmail.com>
2025-10-25 00:20:50 +1300

    End Screen Animation Done

--- auckland-diary-simulator/scripts/EndingScreen.gd
+++ auckland-diary-simulator/scripts/EndingScreen.gd
@@ -1,9 +1,26 @@
 #MADE WITH CHATGPT
 extends Control
-@onready var ending_text = $ending_text
+@onready var day_7: TextureRect = $Day7
+@onready var top: Sprite2D = $Top
+@onready var bottom: Sprite2D = $Bottom
+@onready var ram_raided: TextureRect = $RamRaided
+@onready var return_to_main_menu: Button = $"Return to Main Menu"
 
-func set_text(txt: String):
-	ending_text.text = txt
+func _ready() -> void:
+	await get_tree().create_timer(1.0).timeout
+	top.moveAnimation()
+	bottom.moveAnimation()
+	await get_tree().create_timer(1.0).timeout
+	day_7.modulate.a = 1.0
+	await get_tree().create_timer(3.0).timeout
+	var tween = get_tree().create_tween()
+	tween.tween_property(day_7, "modulate:a", 0, 2.0)
+	await get_tree().create_timer(5.0).timeout
+	ram_raided.modulate.a = 1.0
+	await get_tree().create_timer(5.0).timeout
+	return_to_main_menu.show()
+	
+	# HAVE CHECK THE FLAGS FOR THE ENDING
 
 func _on_return_to_main_menu_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/MainMenu.tscn")
