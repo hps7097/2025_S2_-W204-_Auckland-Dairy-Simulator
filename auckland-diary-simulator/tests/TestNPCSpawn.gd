86c312505d7126576a73a3e0c6e493708cc1d658
hps7097 <hps7097@autuni.ac.nz>
2025-10-24 19:35:56 +1300

    Delete auckland-diary-simulator/tests/TestNPCSpawn.gd

--- auckland-diary-simulator/tests/TestNPCSpawn.gd
+++
@@ -1,9 +0,0 @@
-# MADE WITH CHATGPT
-extends GutTest
-
-func test_police_spawn_condition():
-	GameState.reset()
-	GameState.set_flag("illegal_sold", true)
-	var npc_manager = preload("res://scripts/npc_manager.gd").new()
-	var npc = npc_manager.spawn_npc("police")
-	assert_not_null(npc)
