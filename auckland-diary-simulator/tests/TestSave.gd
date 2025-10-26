3390afa459cb62605a175ca0d0d48dab4db3934c
hps7097 <hps7097@autuni.ac.nz>
2025-10-26 16:30:39 +1300

    Delete auckland-diary-simulator/tests/TestSave.gd

--- auckland-diary-simulator/tests/TestSave.gd
+++
@@ -1,9 +0,0 @@
-# MADE WITH CHATGPT
-extends GutTest
-
-func _ready():
-	var save = preload("res://scripts/SaveSystem.gd").new()
-	save.save_game()
-	save.load_game()
-	assert(save.game_state.money >= 0)
-	get_tree().quit()
