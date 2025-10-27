6f114460e7ca3e73af2f9f31be6cf9fb68bd1787
dhonburi <dhonlao2005@gmail.com>
2025-10-28 09:18:24 +1300

    Delete Redundant Code, ready for merging

--- auckland-diary-simulator/tests/TestUpgrades.gd
+++
@@ -1,9 +0,0 @@
-# MADE WITH CHATGPT
-extends "res://addons/gut/test.gd"
-
-func before_each():
-    GameState.reset_state()
-
-func test_add_upgrade():
-    GameState.add_upgrade("fast_scanner")
-    assert_true(GameState.has_upgrade("fast_scanner"))
